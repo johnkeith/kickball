f4e6a1494bd72f85c96819e070144dc5917e8cb4
Asjed (Azi) Hussain <asjed.hussain@gmail.com>
2014-05-20 12:06:40 -0400

    lots of progress... need to finish positions

--- server.rb
+++ server.rb
@@ -8,13 +8,28 @@ helpers do
     players = []
 
     CSV.foreach('roster.csv',headers:true) do |row|
-      players << {first_name: row['first_name'], last_name: row['last_name'], position: row['position'], team: row['team']}
+      players << {first_name: row['first_name'],
+                  last_name: row['last_name'],
+                  position: row['position'],
+                  team: row['team']
+                }
     end
 
     players
   end
 end
 
-get '/' do
-  erb :index
+get '/teams/:team' do
+  @team = params[:team]
+  @players = parse_roster
+  @team_name_capitalized = @team.split(" ").each { |name| name.capitalize! }.join(" ")
+  erb :teams
 end
+
+get '/positions/:position' do
+  @position = params[:position]
+  @players = parse_roster
+  @position_cap = @position.split(" ").each { |name| name.capitalize! }.join(" ")
+  erb :positions
+end
+
