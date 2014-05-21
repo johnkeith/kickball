0e4e82280e1219f73d1190c6b768884103e5b42e
John Keith <johnkeith@outlook.com>
2014-05-21 08:45:49 -0400

    refactored view code to use find_all method. moved each view into its own folder

--- server.rb
+++ server.rb
@@ -17,26 +17,48 @@ helpers do
 
     players
   end
+
+  # def get_all_team_names
+  #   teams = []
+  #   players = parse_roster
+  #   players.each do |players|
+  #     teams << players[:team] unless teams.include?(players[:team])
+  #   end
+  #   binding.pry
+  #   teams
+  # end
 end
 
 get '/' do
-  "Hi"
+  redirect '/teams'
+end
+
+get '/teams' do
+  @players = parse_roster
+  @teams = []
+  @positions = []
+
+  parse_roster.each do |player|
+    @teams << player[:team] unless @teams.include?(player[:team])
+    @positions << player[:position] unless @positions.include?(player[:position])
+  end
+
+  erb :'teams/index'
 end
 
 get '/teams/:team' do
   @team = params[:team]
-  @players = parse_roster
+  @players = parse_roster.find_all { |player| player[:team] == @team }
   @team_name_capitalized = @team.split(" ").each { |name| name.capitalize! }.join(" ")
-
-
-  erb :teams
+  erb :'teams/show'
 
 end
 
 get '/positions/:position' do
   @position = params[:position]
-  @players = parse_roster
+  @players = parse_roster.find_all { |player| player[:position] == @position }
   @position_cap = @position.split(" ").each { |name| name.capitalize! }.join(" ")
-  erb :positions
+  
+  erb :'positions/show'
 end
 
---
+++ test.rb
@@ -0,0 +1,6 @@
+if 3 > 1
+  puts "Hi"
+else
+  puts "Bye"
+end
+
